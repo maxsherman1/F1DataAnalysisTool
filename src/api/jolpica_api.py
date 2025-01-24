f2ac3354da64a70e6f6e3b536eda4a107c635e68
maxsherman1 <maxs.murry@hotmail.com>
2025-01-24 00:49:13 +0000

    FR1: Simple cache saving implementation

--- src/api/jolpica_api.py
+++ src/api/jolpica_api.py
@@ -1,5 +1,7 @@
+import json
 import logging
 import requests
+from pathlib import Path
 
 # Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -7,17 +9,25 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(level
 # The base URL for the API
 BASE_URL = "https://api.jolpi.ca/ergast/f1/"
 
+CACHE_DIR = Path(__file__).resolve().parent.parent.parent / "data/cache"
+CACHE_DIR.mkdir(parents=True, exist_ok=True)
+
 # Retrieves data from the Jolpica-F1 API
-def get_data(endpoint, params=None):
+def get_data(endpoint, params=None, use_cache=True):
 
     # Add enpoint onto base url
     url = f"{BASE_URL}{endpoint}"
+
+    cache_file = CACHE_DIR / f"{endpoint.replace('/', '_')}"
 
     # Make API call
     try:
         response = requests.get(url, params=params)
         response.raise_for_status()
         data = response.json()
+        if use_cache:
+            cache_data(cache_file, data)
+        logging.info(f"Data from {url} successfully retrieved and cached.")
         return data
 
     except requests.exceptions.RequestException as e:
@@ -25,8 +35,12 @@ def get_data(endpoint, params=None):
         return "Exception request"
 
 
+def cache_data(cache_file, data):
+    with open(cache_file, "w") as f:
+        json.dump(data, f)
+
 def main():
-    print(get_data("constructor.json", {"limit": 1, "offset": 0}))
+    print(get_data("constructors.json", {"limit": 10, "offset": 0}))
 
 if __name__ == "__main__":
     main()
