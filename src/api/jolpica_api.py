d302801ef778a6941492b536fb595bdf1fb34397
maxsherman1 <maxs.murry@hotmail.com>
2025-01-25 16:05:02 +0000

    FR1: Added comments for readability.

--- src/api/jolpica_api.py
+++ src/api/jolpica_api.py
@@ -13,12 +13,15 @@ BASE_URL = "https://api.jolpi.ca/ergast/f1/"
 CACHE_DIR = Path(__file__).resolve().parent.parent.parent / "data/cache"
 CACHE_DIR.mkdir(parents=True, exist_ok=True)
 
-# Retrieves data from the Jolpica-F1 API
 def get_data(endpoint, params={"limit": 30, "offset": 0}, use_cache=True):
+    """
+    Retrieves data from the Jolpica-F1 API with optional caching.
 
-    # Add enpoint onto base url
-    url = f"{BASE_URL}{endpoint}"
-
+    :param endpoint: API endpoint
+    :param params: Query parameters for the API call (e.g., limit and offset)
+    :param use_cache: Whether to use cached data if available
+    :return: JSON response from the API
+    """
     # Extract parameters
     limit = params.get("limit")
     offset = params.get("offset")
@@ -26,36 +29,47 @@ def get_data(endpoint, params={"limit": 30, "offset": 0}, use_cache=True):
     # Cache file name
     file_name = f"{endpoint.replace('/', '_')}_{limit}_{offset}.json"
 
-    if use_cache and file_name in os.listdir(CACHE_DIR):
+    # return cache file if file is found in cache folder
+    if file_name in os.listdir(CACHE_DIR):
         return load_cache(file_name)
+
+    # Add enpoint to the base url
+    url = f"{BASE_URL}{endpoint}"
 
     # Make API call
     try:
+
+        # Get API data and check for errors
         response = requests.get(url, params=params)
         response.raise_for_status()
         data = response.json()
+
+        # Save data to cache file if cache is enabled
         if use_cache:
             cache_data(file_name, data)
+
+        # Return the response in JSON format
         return data
 
+    # Error handling if an error occurs during data retrieval
     except requests.exceptions.RequestException as e:
         logging.error(f"Error retrieving data: \n{e}")
         return "Exception request"
 
 
+# Cache data function (does not check if cache folder is present)
 def cache_data(file_name, data):
     cache_file = f"{CACHE_DIR}/{file_name}"
     with open(cache_file, "w") as f:
         json.dump(data, f)
     logging.info(f"Data cached to {file_name} successfully.")
 
+# Load data function (does not check if cache file is present)
 def load_cache(file_name):
     cache_file = f"{CACHE_DIR}/{file_name}"
     with open(cache_file, "r") as f:
         logging.info(f"Data loaded from {file_name} successfully.")
         return json.load(f)
-
-
 
 def main():
     data = get_data("constructors")
