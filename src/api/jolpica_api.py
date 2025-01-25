c8f1222a7cfcf9b666302b38f90786f7a94b192d
maxsherman1 <maxs.murry@hotmail.com>
2025-01-25 00:09:13 +0000

    FR1: Implemented cache loading for the get_data function

--- src/api/jolpica_api.py
+++ src/api/jolpica_api.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import os
 import requests
 from pathlib import Path
 
@@ -13,17 +14,20 @@ CACHE_DIR = Path(__file__).resolve().parent.parent.parent / "data/cache"
 CACHE_DIR.mkdir(parents=True, exist_ok=True)
 
 # Retrieves data from the Jolpica-F1 API
-def get_data(endpoint, params=None, use_cache=True):
+def get_data(endpoint, params={"limit": 30, "offset": 0}, use_cache=True):
 
     # Add enpoint onto base url
-    url = f"{BASE_URL}{endpoint}.json"
+    url = f"{BASE_URL}{endpoint}"
 
     # Extract parameters
     limit = params.get("limit")
     offset = params.get("offset")
 
     # Cache file name
-    cache_file = CACHE_DIR / f"{endpoint.replace('/', '_')}_{limit}_{offset}.json"
+    file_name = f"{endpoint.replace('/', '_')}_{limit}_{offset}.json"
+
+    if use_cache and file_name in os.listdir(CACHE_DIR):
+        return load_cache(file_name)
 
     # Make API call
     try:
@@ -31,8 +35,7 @@ def get_data(endpoint, params=None, use_cache=True):
         response.raise_for_status()
         data = response.json()
         if use_cache:
-            cache_data(cache_file, data)
-        logging.info(f"Data from {url} successfully retrieved and cached.")
+            cache_data(file_name, data)
         return data
 
     except requests.exceptions.RequestException as e:
@@ -40,16 +43,28 @@ def get_data(endpoint, params=None, use_cache=True):
         return "Exception request"
 
 
-def cache_data(cache_file, data):
+def cache_data(file_name, data):
+    cache_file = f"{CACHE_DIR}/{file_name}"
     with open(cache_file, "w") as f:
         json.dump(data, f)
+    logging.info(f"Data cached to {file_name} successfully.")
+
+def load_cache(file_name):
+    cache_file = f"{CACHE_DIR}/{file_name}"
+    with open(cache_file, "r") as f:
+        logging.info(f"Data loaded from {file_name} successfully.")
+        return json.load(f)
+
+
 
 def main():
-    data = get_data("constructors", {"limit": 10, "offset": 0})
-    for key in data:
-        for x in data[key]:
-            print(x)
+    data = get_data("constructors")
+    #for key in data:
+    #    for x in data[key]:
+    #        print(x)
         #print(key, ":", data[key])
+    #print(data.get("MRData").get("total"))
+    #print(get_all_data("constructors"))
 
 if __name__ == "__main__":
     main()
