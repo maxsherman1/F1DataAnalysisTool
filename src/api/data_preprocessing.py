a46898a77558cf6a1d3d84f4e5bc20998414a21c
maxsherman1 <maxs.murry@hotmail.com>
2025-02-02 22:31:29 +0000

    FR1 & FR5: Added comments and altered the convert and save function

--- src/api/data_preprocessing.py
+++ src/api/data_preprocessing.py
@@ -13,28 +13,34 @@ CLEANED_DIR.mkdir(parents=True, exist_ok=True)  # Ensure the processed directory
 
 # Converts the provided list to a pandas dataframe
 def convert_to_dataframe(data: List) -> pd.DataFrame:
+    # Checking for data
     if not data:
         logging.warning("No data provided for conversion to DataFrame. Returning empty DataFrame.")
         return pd.DataFrame()
+
+    # Normalise JSON data
     try:
         return pd.json_normalize(data)
+    # Log an error if the conversion fails
     except Exception as e:
         logging.error("Error converting data to DataFrame: %s", str(e))
         return pd.DataFrame()
 
 # Save the dataframe to the file path
 def save_data(data: pd.DataFrame, file_path: Path) -> None:
+    # Check if there is data to save
     if data.empty:
         logging.warning("No data provided for saving.")
         return
+
+    # Save data to csv at file_path
     try:
         data.to_csv(file_path, index=False)
         logging.info("Saved cleaned data to %s", file_path)
+    # Log an error if saving fails
     except Exception as e:
         logging.error("Error saving cleaned data: %s", str(e))
 
-def clean_and_save_data(resource_type:str, **filters):
-    jolpica_api = JolpicaAPI(resource_type, **filters)
-    data = convert_to_dataframe(jolpica_api.get_inner_data())
-    file_name = f"{jolpica_api.build_endpoint().replace('/', '_')}_cleaned.csv"
-    save_data(data=data, file_path=CLEANED_DIR / file_name)
+def convert_and_save_data(jolpica_api: JolpicaAPI) -> None:
+    file_name = jolpica_api.get_cleaned_file_name()
+    save_data(convert_to_dataframe(jolpica_api.get_inner_data()), CLEANED_DIR / file_name)
