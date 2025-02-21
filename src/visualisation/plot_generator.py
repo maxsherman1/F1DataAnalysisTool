460918a3fdb38e96d8e1ecf415c605e6fcd9ec9c
maxsherman1 <maxs.murry@hotmail.com>
2025-02-21 01:37:04 +0000

    FR2: refactored the plot generator code

--- src/visualisation/plot_generator.py
+++ src/visualisation/plot_generator.py
@@ -1,9 +1,12 @@
-import matplotlib.pyplot as plt
 import pandas as pd
 from api.jolpica_api import JolpicaAPI
 from visualisation.static_plot import plot_static_chart
 from visualisation.interactive_plot import plot_interactive_chart
 from visualisation.plot_saving import save_plot, get_plots_directory
+
+def generate_filename(mode: str, chart_type: str, x_col: str, y_col: str, title: str) -> str:
+    filename = f"{mode}_{chart_type}_{x_col.replace('.', '_')}{'_' + y_col.replace('.', '_') if y_col else ''}_{title.replace(' ', '_')}"
+    return filename.lower()
 
 def plot_chart(
         df: pd.DataFrame, x_col: str, y_col: str = None, title: str = "",
@@ -11,26 +14,21 @@ def plot_chart(
 ):
     # Extract mode and specific plot type
     mode, chart_type = plot_type
-
     default_format = "html" if mode == "interactive" else "png"
     save_format = save_format if save_format else default_format
 
     # Construct filename for caching
-    filename = f"{mode}_{chart_type}_{x_col.replace('.', '_')}{'_' + y_col.replace('.', '_') if y_col else ''}_{title.replace(' ', '_')}"
-    filename = filename.lower()
+    filename = generate_filename(mode, chart_type, x_col, y_col, title)
     save_path = get_plots_directory() / f"{filename}.{save_format}"
 
-    # Generate new plot if not already saved
+    # Generate plot
     if mode == "static":
-        plot_static_chart(df, x_col=x_col, y_col=y_col, title=title,
-                          plot_type=chart_type, **kwargs)
-        if saving and not save_path.exists():
-            save_plot(plt.gcf(), filename=filename, plot_type="static", file_format=save_format)
-        return plt.gcf()
-
-    elif mode == "interactive":
+        fig = plot_static_chart(df, x_col=x_col, y_col=y_col, title=title,
+                                plot_type=chart_type, **kwargs)
+    else:
         fig = plot_interactive_chart(df, x_col=x_col, y_col=y_col, title=title,
                                      plot_type=chart_type, **kwargs)
-        if saving and not save_path.exists():
-            save_plot(fig, filename=filename, plot_type="interactive", file_format=save_format)
-        return fig
+    if saving and not save_path.exists():
+        save_plot(fig, filename=filename, plot_type=mode, file_format=save_format)
+
+    return fig
